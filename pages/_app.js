6b11bab91b9d079e7f427462ce77680e08a64071
Temitope Akande <topsiology11@gmail.com>
2023-05-20 16:03:19 +0100

    debug

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
  
- import { Provider } from "@self.id/react"; 
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { Provider } from '@self.id/react'
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }) {
-  return 
- <Provider client={{ ceramic: "testnet-clay" }} >
-      <Component {...pageProps} />;
-    </Provider>
-
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider client={{ ceramic: "testnet-clay" }}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
+  )
 }
 
 export default MyApp
-
